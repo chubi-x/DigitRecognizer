55d95d6465c4a0c9709d5dfe149bdf7a5e8a1b15
chubi-x <michaeladejoh01@gmail.com>
2021-02-28 21:56:20 +0400

    complete neural network architecture with testing and training implemented

--- Network/src/network/Network.java
+++ Network/src/network/Network.java
@@ -17,119 +17,220 @@ import org.apache.commons.math3.util.Precision;
  */
 public class Network {
 
-    /**
-     * @param args the command line arguments
-     */
-    private static double inputs[] = new double[65];
-    private static double desiredOutput[] = new double[10];
-    private static double hiddenLayerWeights[][] = new double[2000][64]; //ten sets of weights with 64 values each
-    private static double hiddenLayerOutputs[] = new double[2000]; //ten outputs
-    private static double outputLayerWeights[][] = new double[10][2000]; //ten sets of weights with 10 values each
-    private static double outputLayerOutputs[] = new double[10]; //ten outputs
-    private static float success = 0;
-    private static float accuracy = 0;
-    private static float inputCount = 0;
-    private static int epochs = 20;
+    private static int desiredOutputsArray[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private static double hiddenLayerWeights[][] = new double[3][64]; //ten sets of weights with 64 values each
+    private static double outputLayerWeights[][] = new double[10][3]; //ten sets of weights with 10 values each
+
+    private static double trainingInputs[] = new double[65];
+    private static double trainingHiddenLayerOutputs[] = new double[3]; //ten outputs
+    private static double trainingOutputLayerOutputs[] = new double[10]; //ten outputs
+    private static int trainingTempOutputLayerOutputs[] = new int[10];  //array to hold temporary values from sigmoid
+
+    private static double testingInputs[] = new double[65];
+    private static double testingHiddenLayerOutputs[] = new double[3]; //ten outputs 
+    private static double testingOutputLayerOutputs[] = new double[10]; //ten outputs
+    private static int testingTempOutputLayerOutputs[] = new int[10];  //array to hold temporary values from sigmoid
+
+    private static double trainingSuccess = 0;
+    private static double testingSuccess = 0;
+    private static double inputCount = 2810;
+    private static int epochs = 7;
     private static double learningRate = 0.005;
+    private static double trainingAccuracy = 0;
+    private static double testingAccuracy = 0;
 
     public static void main(String[] args) {
-        //read inputs file
-        try {
-            File file = new File("C:\\Users\\PC\\Documents\\NetBeansProjects\\Coursework2\\training.csv");
-            Scanner reader = new Scanner(file);
 
-            //populate input hidden layer weights
-            for (double[] weights : hiddenLayerWeights) {
-                populateWeights(weights);
-            }
-            //populate output layer weights
-            for (double[] weights : outputLayerWeights) {
-                populateWeights(weights);
-            }
+        //populate input hidden layer weights
+        populateAllWeights();
 
-            //start computing
-            int observations = 0;
-            while (observations < epochs) {
+        //start computing
+        int cycles = 0;
+        //file reading should be inside
+        while (cycles < epochs) {
+            //read inputs file
+            try {
+                File TestingFile = new File("C:\\Users\\PC\\Documents\\NetBeansProjects\\Coursework2\\training.csv");
                 //loop through the file
                 // <editor-fold defaultstate="collapsed" desc="file reader while loop ">
+                Scanner reader = new Scanner(TestingFile);
+                // <editor-fold defaultstate="collapsed" desc="file reader while loop ">
                 while (reader.hasNextLine()) {
-                    //read the line of the file
-                    String features = reader.nextLine();
-                    //split the line into an arrray of features
-                    String[] featuresOutput = features.split(",");
-                    //convert features to double and populate inputs array
-                    for (int i = 0; i < featuresOutput.length; i++) {
-                        inputs[i] = Double.parseDouble(featuresOutput[i]);
-                    }
-
+                    //read file
+                    readFile(reader, trainingInputs);
                     //retrieve the desired digits from inputs array
-                    int digit = (int) inputs[inputs.length - 1];
+                    //rename todesired output
+                    int digit = (int) trainingInputs[trainingInputs.length - 1];
                     //create mapping for desired outputs
-                    mapping(digit, desiredOutput);
-
+                    mapping(digit);
                     //loop through the hidden layer weights
                     //calculate the summation of the inputs to get the hidden layer outputs
-                    int i = 0;
-                    for (double[] weights : hiddenLayerWeights) {
-                        //calculate the summation of each neuron
-                        double summation = summation(inputs, weights);
-                        //populate the hidden layer outputs array
-                        hiddenLayerOutputs[i] = summation;
-                        i++;
-                    }
-
+                    hiddenLayerSummation(trainingInputs, trainingHiddenLayerOutputs);
                     //calculate the output layer outputs
-                    int a = 0;
-                    for (double weights[] : outputLayerWeights) {
-                        //calculate the summation between the hidden layer neurons and the output layer neurons
-                        double summation = summation(hiddenLayerOutputs, weights);
-                        //apply the sigmoid function
-                        int neuronOutput = sigmoidFunction(summation);
-                        //populate the outputlayer output
-                        outputLayerOutputs[a] = neuronOutput;
-                        a++;
-                    }
-                    if (Arrays.equals(outputLayerOutputs, desiredOutput)) {
-                        success++;
+                    outputLayerOutput(trainingHiddenLayerOutputs, trainingOutputLayerOutputs);
+                    //populate temporary output layer outputs
+                    tempOutputLayerOutput(trainingOutputLayerOutputs, trainingTempOutputLayerOutputs);
+                    //network training
+                    //check success rate
+                    if (Arrays.equals(trainingTempOutputLayerOutputs, desiredOutputsArray)) {
+                        trainingSuccess++;
                     } else {
-                        //calculate the error in each outputlayer neuron
-                        //loop through the output layer outputs
-                        for (int b = 0; b < outputLayerOutputs.length; b++) {
-                            //compare each value with the desired outputs mapping to find the error
-                            if (outputLayerOutputs[b] != desiredOutput[b]) {
-                                //calculate the error for each output layer neuron
-                                double outputLayerNeuronError = desiredOutput[b] - outputLayerOutputs[b];
-                                //calculate the error for each hidden layer neuron
-                                for (int k = 0; k < outputLayerWeights.length; k++) {
-                                    double hiddenLayerNeuronError = outputLayerOutputs[b] * (1 - outputLayerOutputs[b]) * outputLayerWeights[b][k] * outputLayerNeuronError;
-                                    //adjust the output layer weights
-                                    adjustWeights(hiddenLayerOutputs, outputLayerWeights[b], learningRate, outputLayerNeuronError);
-                                    //adjust the hidden layer weights
-                                    adjustWeights(inputs, hiddenLayerWeights[b], learningRate, hiddenLayerNeuronError);
-                                }
+                        networkTraining();
+                    }
 
-                            }
-                        }
+                    //clear desiredOutput array
+                    clearDesiredOutput();
+                }
+//                close the scanner
+                reader.close();
+
+            } catch (FileNotFoundException e) {
+                System.out.println("Error reading file " + e);
+            }
+//            inputCount = 0;
+            cycles++;
+        }
+        cycles = 0;
+        //clear desiredOutput array
+        clearDesiredOutput();
+
+        //</editor-fold>
+        System.out.println("count and success are " + inputCount + " and  " + trainingSuccess);
+        trainingAccuracy = trainingSuccess / inputCount;
+        System.out.println(" Training Accuracy is " + trainingAccuracy * 100);
+
+        System.out.println("\n\n################################\n TESTING \n#################################");
+        while (cycles < epochs) {
+            //retrieve testing file
+            try {
+                File trainingFile = new File("C:\\Users\\PC\\Documents\\NetBeansProjects\\Coursework2\\testing.csv");
+                //read testing file
+                Scanner reader = new Scanner(trainingFile);
+                while (reader.hasNextLine()) {
+                    //convert the file to double array
+                    readFile(reader, testingInputs);
+                    //retrieve desired output
+                    int digit = (int) testingInputs[testingInputs.length - 1];
+                    //desired output mapping
+                    mapping(digit);
+                    //calculate the summation of the inputs to get the hidden layer outputs
+                    hiddenLayerSummation(testingInputs, testingHiddenLayerOutputs);
+                    //calculate the output layer outputs
+                    outputLayerOutput(testingHiddenLayerOutputs, testingOutputLayerOutputs);
+                    //populate temporary output layer outputs
+                    tempOutputLayerOutput(testingOutputLayerOutputs, testingTempOutputLayerOutputs);
+                    //calculate success
+                    if (Arrays.equals(testingTempOutputLayerOutputs, desiredOutputsArray)) {
+                        testingSuccess++;
                     }
                     //clear desiredOutput array
-                    desiredOutput = new double[10];
-                    inputCount++;
-
+                    clearDesiredOutput();
                 }
 
-                observations++;
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found" + e);
             }
-            //</editor-fold>
-            System.out.println("count and success are " + inputCount + " and  " + success);
-            System.out.println("Accuracy is " + success / inputCount * 100);
+            cycles++;
+        }
+        System.out.println("count and success are " + inputCount + " and  " + testingSuccess);
+        testingAccuracy = testingSuccess / inputCount;
+        System.out.println(" Testing Accuracy is " + testingAccuracy * 100);
+    }
 
-        } catch (FileNotFoundException e) {
-            System.out.println("Error reading file " + e);
+    public static void populateAllWeights() {
+        for (double[] weights : hiddenLayerWeights) {
+            populateWeights(weights);
+        }
+        //populate output layer weights
+        for (double[] weights : outputLayerWeights) {
+            populateWeights(weights);
         }
     }
 
-    public static void mapping(int digit, double[] array) {
-        array[digit] = 1;
+    public static void readFile(Scanner reader, double[] inputs) {
+        //read the line of the file
+        String features = reader.nextLine();
+        //split the line into an arrray of features
+        String[] featuresOutput = features.split(",");
+        //convert features to double and populate inputs array
+        for (int i = 0; i < featuresOutput.length; i++) {
+            inputs[i] = Double.parseDouble(featuresOutput[i]);
+        }
+    }
+
+    public static void mapping(int digit) {
+        desiredOutputsArray[digit] = 1;
+    }
+
+    public static void hiddenLayerSummation(double[] inputs, double[] outputs) {
+        int i = 0;
+        for (double[] weights : hiddenLayerWeights) {
+            //calculate the summation of each neuron
+            double summation = summation(inputs, weights);
+            //populate the hidden layer outputs array
+            outputs[i] = sigmoidFunction(summation);
+            i++;
+        }
+        i = 0;
+    }
+
+    public static void outputLayerOutput(double[] hiddenLayerOutputs, double[] outputLayerOutputs) {
+        int a = 0;
+        for (double weights[] : outputLayerWeights) {
+            //calculate the summation between the hidden layer neurons and the output layer neurons
+            double summation = summation(hiddenLayerOutputs, weights);
+            //apply the sigmoid function
+            double neuronOutput = sigmoidFunction(summation);
+            //populate the outputlayer output
+            outputLayerOutputs[a] = neuronOutput;
+            a++;
+        }
+        a = 0;
+    }
+
+    public static void tempOutputLayerOutput(double[] outputs, int[] tempOutputs) {
+        //find the maximum value of the outputlayer outputs and set it to 1 and the others to 0
+        double max = 0;
+        for (int r = 0; r < outputs.length; r++) {
+            if (outputs[r] >= max) {
+                max = outputs[r];
+            }
+        }
+        for (int q = 0; q < outputs.length; q++) {
+            if (outputs[q] >= max) {
+                tempOutputs[q] = 1;
+            } else {
+                tempOutputs[q] = 0;
+            }
+        }
+    }
+
+    public static void networkTraining() {
+        //calculate the error in each outputlayer neuron
+        //loop through the output layer outputs
+        for (int b = 0; b < trainingOutputLayerOutputs.length; b++) {
+            //compare each value with the desired outputs mapping to find the error
+            if (trainingOutputLayerOutputs[b] != desiredOutputsArray[b]) {
+                //calculate the error for each output layer neuron
+                double outputLayerNeuronError = desiredOutputsArray[b] - trainingOutputLayerOutputs[b];
+                //calculate the error for each hidden layer neuron
+                for (int k = 0; k < trainingHiddenLayerOutputs.length; k++) {
+                    //hidden layer neuron error is always 0 since output layer outputs is binary
+                    double hiddenLayerNeuronError = trainingOutputLayerOutputs[b] * (1 - trainingOutputLayerOutputs[b]) * outputLayerWeights[b][k] * outputLayerNeuronError;
+                    //adjust the output layer weights
+                    adjustWeights(trainingHiddenLayerOutputs, outputLayerWeights[b], learningRate, outputLayerNeuronError);
+                    //adjust the hidden layer weights
+                    adjustWeights(trainingInputs, hiddenLayerWeights[k], learningRate, hiddenLayerNeuronError);
+                }
+
+            }
+        }
+    }
+
+    public static void clearDesiredOutput() {
+        for (int val = 0; val < desiredOutputsArray.length; val++) {
+            desiredOutputsArray[val] = 0;
+        }
     }
 
     public static void populateWeights(double[] weights) {
@@ -159,9 +260,9 @@ public class Network {
         return sum;
     }
 
-    public static int sigmoidFunction(double sum) {
-        int result;
-        result = (int) (1 / (1 + Math.pow(Math.E, -1 * sum)));
+    public static double sigmoidFunction(double sum) {
+        double result;
+        result = 1 / (1 + Math.pow(Math.E, -1 * sum));
         return result;
     }
 
